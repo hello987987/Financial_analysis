33fa8184b700176928e970361db30399ae598846
Sebastian Robak <robaksebastian77@gmail.com>
2026-01-28 19:27:06 +1100

    removed unnessisary response variable

--- scraper/html_fetcher.py
+++ scraper/html_fetcher.py
@@ -31,7 +31,6 @@ class HtmlFetcher:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
-            response = page.goto(url, wait_until="domcontentloaded", timeout=15000)
             html = page.content()
             browser.close()
             return html
