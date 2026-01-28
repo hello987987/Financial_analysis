425ecbf3a572125a068f6af50b6447b6f861c81d
Sebastian Robak <robaksebastian77@gmail.com>
2026-01-28 19:28:13 +1100

    created story registry class

---
+++ scraper/story_registry.py
@@ -0,0 +1,14 @@
+class StoryRegistry:
+    def __init__(self):
+        self.story_registry = []
+
+    def append(self, site_origin, headline, description, url):
+        self.story_registry.append({
+                "headline": headline if headline else "NO_HEADLINE",
+                "site_origin" : site_origin if site_origin else "UNKNON_ORIGIN",
+                "desc": description if description else "NO_DESCRIPTION",
+                "url": url if url else "NO_URL"
+        })
+    
+    def get_story_registry(self):
+        return self.story_registry
