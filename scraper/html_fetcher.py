f7fec51ccbf8be4f95f4c09b813680b2b662d224
Sebastian Robak <sebastian.p.robak@student.uts.edu.au>
2026-01-29 17:53:47 +1100

    Improved html_fetcher readability and handle requests/playwright exceptions explicity

--- scraper/html_fetcher.py
+++ scraper/html_fetcher.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import sync_playwright
-import requests
+from playwright.sync_api import TimeoutError as PlaywrightTimeout
 
+import requests
+from requests import RequestException
 class HtmlFetcher:
     def __init__(self):
         self.headers = {
@@ -12,26 +14,28 @@ class HtmlFetcher:
             "Accept-Language": "en-AU,en;q=0.9"
         }
 
-    def fetch(self, url:str):# TODO: make this try headless if bad return code.
+    def fetch(self, url: str) -> str:
         try:
             http_response = self._request_get(url)
-            if http_response and http_response.ok:
+            
+            if http_response.ok:
                 return http_response.text
 
             return self._headless_get(url)
-        
-        except Exception:
+
+        except (RequestException, PlaywrightTimeout):
             return ""
 
-    def _request_get(self, url:str):
+    def _request_get(self, url:str) -> requests.Response:
         http_response = requests.get(url, timeout=10)  
         return http_response
 
-    def _headless_get(self, url:str):
+    def _headless_get(self, url: str) -> str:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
-            page.goto(url, wait_until="domcontentloaded", timeout=15000)
-            html = page.content()
-            browser.close()
-            return html
+            try:
+                page = browser.new_page()
+                page.goto(url, wait_until="domcontentloaded", timeout=15000)
+                return page.content()
+            finally:
+                browser.close()
